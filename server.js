10e4ecc1a7d4803b1b1edbbebe4df8799865a9a1
川瀬拓実 <coil_the-world_1341398@i.softbank.jp>
2015-11-17 09:53:15 +0000

    recentだけちょこっとapi作った

--- server.js
+++ server.js
@@ -1,107 +1,33 @@
-#!/usr/bin/env node
+var express = require('express'),
+    path = require('path'),
+    http = require('http'),
+    Recent_api = require('./routes/recent');
+    //User_api = require('./routes/token'),
+    //Room_api = require('./routes/room');
 
-var http = require("http"),
-    url  = require("url"),
-    path = require("path"),
-    fs   = require("fs");
+var app = express();
 
-var port = process.env.PORT || 1337;
+app.configure(function () {
+    app.set('port', process.env.PORT || 3000);
+    app.use(express.logger('dev'));  /* 'default', 'short', 'tiny', 'dev' */
+    app.use(express.bodyParser()),
+    app.use(express.static(path.join(__dirname, 'public')));
+});
 
-(function(root, port) {
-
-  //
-  // status code message map
-  //
-  var message = {
-    200: "OK",
-    404: "Not Found",
-    500: "Internal Server Error",
-    501: "Note Implemented"
-  };
-
-  //
-  // mime type map
-  //
-  var mime = {
-    ".html": "text/html",
-    ".css":  "text/css",
-    ".js":   "application/javascript",
-    ".png":  "image/png",
-    ".jpg":  "image/jpeg",
-    ".gif":  "image/gif",
-    ".txt":  "text/plain"
-  };
-
-  //
-  // send requested file
-  //
-  function sendFile(req, res, filePath) {
-
-    var file = fs.createReadStream(filePath);
-    file.on("readable", function() {
-      res.writeHead(200, {"Content-Type": mime[path.extname(filePath)] || "text/plain"});
-    });
-
-    file.on("data", function(data) {
-      res.write(data);
-    });
-
-    file.on("close", function() {
-      res.end();
-      console.log("<- " + message[200] + ": " + req.method + " " + req.url);
-    });
-
-    file.on("error", function(err) {
-      sendError(req, res, 500);
-    });
-  }
-
-  //
-  // send error status
-  //
-  function sendError(req, res, statusCode) {
-    res.writeHead(statusCode, {"Content-Type": "text/html"});
-    res.write("<!DOCTYPE html><html><body><h1>" + message[statusCode] + "</h1></body></html>");
-    res.end();
-    console.log("<- " + message[statusCode] + ": " + req.method + " " + req.url);
-  }
-
-  //
-  // request handler
-  //
-  function handleRequest(req, res, filePath) {
-
-    fs.stat(filePath, function(err, stats) {
-      if (err) {
-        if ((/ENOENT/).test(err.message)) return sendError(req, res, 404);
-        else return sendError(req, res, 500);
-      }
-
-      if (stats.isDirectory())
-        return handleRequest(req, res, path.join(filePath, "index.html")); // try to handle request with index.html file
-      else
-        return sendFile(req, res, filePath);
-    });
-  }
-
-  //
-  // create and start the server
-  //
-  http.createServer(function(req, res) {
-
-    var pathName = url.parse(req.url).pathname;
-    console.log("-> " + req.method + " " + pathName);
-
-    if (req.method === "GET")
-      handleRequest(req, res, path.join(root, pathName));
-    else
-      sendError(req, res, 501);
-
-  }).listen(port);
-
-  //
-  // initiation log
-  //
-  console.log("Http Server running at http://localhost:" + port + "/ (" + root + ")");
-
-}("./",port));
+app.post('/recent/create',Recent_api.create);
+app.delete('/recent/delete/all',Recent_api.deletetable);
+app.delete('/recent/delete/:id',Recent_api.delete);
+app.get('/recent',Recent_api.getAll);
+app.get('/recent/:id',Recent_api.get);
+/*
+app.get('/recent',Recent_api.hage);
+app.post('/recent/create',Recent_api.create);
+app.get('/recent/display',Recent_api.get);
+app.post('/token/create',User_api.create_token);
+app.post('/room/index',Room_api.get_list);
+app.post('/room/create',Room_api.create_room);
+app.post('/room/detail',Room_api.get_detail);
+*/
+http.createServer(app).listen(app.get('port'), function () {
+    console.log("Express server listening on port " + app.get('port'));
+});
