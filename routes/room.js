8523a75cd76eacee9deedc16c6fce05a718d68e1
vagrant <vagrant@localhost.localdomain>
2015-11-19 06:08:16 +0100

    implemented room/create room/index

--- routes/room.js
+++ routes/room.js
@@ -9,44 +9,113 @@ var tableSvc = azure.createTableService(
 
 
 tableSvc.createTableIfNotExists('roomtable',function(err,created){
-    console.log('The roomtable got prepared.');
+    console.log('The roomtable is ready.');
 });
 
 var entGen = azure.TableUtilities.entityGenerator;
 
+var ROOMPARTITIONKEY = 'room';
 
-exports.create_room = function(req,res){
-    console.log(req.body.id + ' : ' + req.body.description);
-    id = req.body.id;
-    description = req.body.description;
-    var task = {
-        PartitionKey: entGen.String('room'),
-        RowKey: entGen.String(id),
-        description: entGen.String(description)
-    };
+var room = (function(){
 
-    tableSvc.insertEntity('roomtable',task,function(error,result,res){
-        if(error){
-            tableSvc.updateEntity('roomtable',task,function(error,result,res){
-                console.log('The data updated.');
-            })
+    var room_number,
+        room_name,
+        room_type,
+        room_member,
+        room_maxMember;
+
+    var room = function(data){
+        console.log('data: ' + data);
+        if(data){
+            this.room_number    = data.room_number;
+            this.room_name      = data.room_name;
+            this.room_type      = data.room_type;
+            this.room_member    = 1;
+            this.room_maxMember = data.room_maxMember;
         }
-        if(!error){
-            console.log('The data inserted.');
-        }
-    });
+    }
+    return room;
+})();
 
-    res.send('Completed.');
+var objectizeRooms = function(rooms){
+    var data = new Array();
+    for(var i = 0 ; rooms.length ; i++){
+        if(i == rooms.length)break;
+        data[i] = new room();
+        data[i].room_number    = rooms[i].RowKey._;
+        data[i].room_name      = rooms[i].RoomName._;
+        data[i].room_type      = rooms[i].RoomType._;
+        data[i].room_mem       = rooms[i].RoomMember._;
+        data[i].room_maxMember = rooms[i].RoomMaxMember._;
+    }
+    return data;
 };
 
 exports.get_list = function(req,res){
     tableSvc.queryEntities('roomtable',null,null,function(error,result){
         if(!error){
             console.log('The data displayed.');
-            data = result.entries;
-            console.log(data);
+            res.send(JSON.stringify(objectizeRooms(result.entries)));
         }
-        res.send(data);
+    });
+};
+
+var createEntity = function(room){
+
+    var entity = {
+        PartitionKey  : entGen.String(ROOMPARTITIONKEY),
+        RowKey        : entGen.Int32(room.room_number),
+        RoomName      : entGen.String(room.room_name),
+        RoomType      : entGen.String(room.room_type),
+        RoomMember    : entGen.Int32(room.room_member),
+        RoomMaxMember : entGen.Int32(room.room_maxMember)
+    };
+
+    return entity;
+};
+
+var sender = function(res,result){
+    res.send(result);
+};
+
+var roomData = function(room_number,res,sender){
+    var query;
+    if(room_number){
+        query = new azure.TableQuery()
+        .where('RowKey eq ?',room_number);
+    }else{
+        query = null;
+    }
+
+    tableSvc.queryEntities('roomtable',query,null,function(error,result){
+        if(!error){
+            sender(JSON.stringify(objectizeRooms(result.entries)));
+        }
+        if(error){
+            console.log('query error');
+        }
+    })
+}
+
+exports.create = function(req,res){
+    console.log(req.body);
+    var newRoom = new room(req.body);
+
+    entity = createEntity(newRoom);
+
+    tableSvc.insertEntity('roomtable',entity,function(error,result){
+        if(error){
+            // 呼び出されるとまずい
+            tableSvc.updateEntity('roomtable',entity,function(error,result){
+                console.log('The data updated.');
+            })
+        }
+        if(!error){
+            console.log('The data inserted.');
+        }
+        roomData(newRoom.room_number,res,function(result){
+            res.send(result);
+        });
     });
 };
 
--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 var express = require('express'),
     path = require('path'),
     http = require('http'),
-    Recent_api = require('./routes/recent');
-    User_api = require('./routes/token'),
     Room_api = require('./routes/room');
 
 var app = express();
@@ -14,32 +12,12 @@ app.configure(function () {
     app.use(express.static(path.join(__dirname, 'public')));
 });
 
-// APIs for /recent
-app.post('/recent/create',Recent_api.create);
-app.delete('/recent/delete/all',Recent_api.deletetable);
-app.delete('/recent/delete/:id',Recent_api.delete);
-app.get('/recent',Recent_api.getAll);
-app.get('/recent/:id',Recent_api.get);
-
-// An API for /token
-app.post('/token/create',User_api.create_token);
-app.get('/token/index',User_api.get_list);
-app.delete('/token/delete/all',User_api.deletetable);
-
 // APIs for /room
-app.post('/room/index',Room_api.get_list);
-app.post('/room/create',Room_api.create_room);
-app.post('/room/detail',Room_api.get_detail);
+app.get('/room/index',Room_api.get_list);
+app.post('/room/create',Room_api.create);
+//app.post('/room/out',Room_api.out);
+app.delete('/room/deletetable',Room_api.deletetable);
 
-/*
-app.get('/recent',Recent_api.hage);
-app.post('/recent/create',Recent_api.create);
-app.get('/recent/display',Recent_api.get);
-app.post('/token/create',User_api.create_token);
-app.post('/room/index',Room_api.get_list);
-app.post('/room/create',Room_api.create_room);
-app.post('/room/detail',Room_api.get_detail);
-*/
 http.createServer(app).listen(app.get('port'), function () {
     console.log("Express server listening on port " + app.get('port'));
 });
