d586f6847f13ad2aa4e3519572bd5e0e76b1a18e
LimeStreem <LimeStreem@gmail.com>
2015-12-13 00:47:28 -0800

    add animation

---
+++ BomberMan/Assets/Scripts/Character/PlayerCharacterMotionProvider.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCharacterMotionProvider : CharacterMotionProvider {
+	protected override Vector2 getMovedVector ()
+	{
+		float horizontal = Input.GetAxisRaw ("Horizontal");
+		float vertical = Input.GetAxisRaw ("Vertical");
+		if (horizontal != 0)
+			return new Vector2 (horizontal, 0);
+		else
+			return new Vector2 (0, vertical);
+	}
+}
