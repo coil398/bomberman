ef05032e4acac97f40d2bdbc1026f157c1be721b
川瀬拓実 <coil_the-world_1341398@i.softbank.jp>
2015-12-14 01:16:43 +0000

    適当に変更

---
+++ BomberMan/Assets/Scripts/Room/TestButtonScript.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestButtonScript : MonoBehaviour {
+
+	public void ClickToDisplayOnConsole(){
+		Debug.Log("The button clicked");
+	}
+}
---
+++ BomberMan/Assets/Scripts/SelectRoom/MoveToRoom1.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class MoveToRoom1 : MonoBehaviour {
+
+	public void ButtonPush()
+	{
+		SelectRoom room = new SelectRoom(1);
+	}
+}
