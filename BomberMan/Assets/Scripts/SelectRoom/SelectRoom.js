aa506aafc612fe9038f36da25ed318bc1e238af9
川瀬拓実 <coil_the-world_1341398@i.softbank.jp>
2015-12-16 22:37:41 +0000

    通信したデータの処理方法わからん

--- BomberMan/Assets/Scripts/SelectRoom/SelectRoom.js
+++ BomberMan/Assets/Scripts/SelectRoom/SelectRoom.js
@@ -1,27 +1,20 @@
-var connectionToNode;
+var p;
 
 function Start () {
-    Application.targetFrameRate = 0.1;
-    connectionToNode = GetComponent("ConnectionToNode");
+    p = GetComponent("ConnectionToNode");
 }
 
-function CallForRoomList(){
-    connectionToNode.writeSocket("CallForRoomList");
-}
 
 function Update () {
-    Debug.Log(connectionToNode);
-    if(connectionToNode){
-        CallForRoomList();
-    }
 }
 
 function OnGUI(){
     if(GUI.Button(Rect(200,40,200,60),"Send")){
-        connectionToNode.writeSocket("test");
+        p.writeSocket("test");
     }
 
     if(GUI.Button(Rect(200,120,200,60),"Send")){
+        print(p.readSocket());
     }
 
     if(GUI.Button(Rect(200,200,200,60),"Send")){
